ae1334bb24e87681810abdb3c8e602c104973623
带dai <dai.0v0@qq.com>
2024-06-25 20:39:21 +0800

    fix package

--- src/main/java/cn/edu/hit/rcare/config/testCorsConfiguration.java
+++ src/main/java/cn/edu/hit/rcare/config/testCorsConfiguration.java
@@ -1,4 +1,4 @@
-package code.space.codespace.config;
+package cn.edu.hit.rcare.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
